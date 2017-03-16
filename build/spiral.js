a23790a744e8a23fdfb4ea4a62a3462911370667
Pesah Arthur <arthur.pesah@ensta-paristech.fr>
2017-03-16 20:38:50 +0100

    From svg to canvas

--- build/spiral.js
+++ build/spiral.js
@@ -101,62 +101,66 @@ function SpiralCreator(div) {
     this.params =   {
         e: {label: "Excentricity", value: 1.3, range:[0.1, 10], scale: d3.scaleLog(), ticks: 2, decimals: 2},
         noise: {label: "Noise", value: 0.5, range: [0.1, 10], scale: d3.scaleLog(), ticks: 2, decimals: 2},
-        nbrStarsInEllipse: {label: "Number of stars per ellipse", value: 200, range: [50, 500], scale: d3.scaleLinear(), ticks: 5, decimals: 0},
-        nbrEllipses: {label: "Number of ellipses", value: 60, range: [10, 100], scale: d3.scaleLinear(), ticks: 10, decimals: 0}
+        nbrStarsInEllipse: {label: "Number of stars per ellipse", value: 150, range: [50, 500], scale: d3.scaleLinear(), ticks: 5, decimals: 0},
+        nbrEllipses: {label: "Number of ellipses", value: 40, range: [10, 100], scale: d3.scaleLinear(), ticks: 10, decimals: 0}
     };
 
     this.margin = 25;
     this.widthCurve = this.myGalaxyDiv.nodes()[0].offsetWidth;
     this.heightCurve = 400;
-    this.colorPoint = "rgba(255, 245, 242,1.0)";
-    this.sizePoint = 15;
+    this.colorPoint = "#DBE3EB";
+    this.sizePoint = 15; // 15
     this.bgColor = '#080810';
+
+    this.canvas = this.myGalaxyDiv.append("canvas")
+    .attr("width", this.widthCurve).attr("height", this.heightCurve)
+    .attr("style", "background-color: " + this.bgColor);
+    this.context = this.canvas.node().getContext("2d");
 
     this.displayParams();
     this.displayStars();
 }
 
-SpiralCreator.prototype.createGradientColor = function(svg) {
-    var gradient = svg.append("svg:defs")
-    .append("svg:radialGradient")
-    .attr("id", "gradientStar")
-    .attr("cx", 0.25)
-    .attr("cy", 0.25)
-    .attr("r", 0.25);
+SpiralCreator.prototype.createGradientColor = function() {
+    var colorToRgba = function(hex, opacity) {
+        var rgbColor = "rgba(";
+        rgbColor = rgbColor.concat(parseInt(hex.slice(1,3), 16), ",");
+        rgbColor = rgbColor.concat(parseInt(hex.slice(3,5), 16), ",");
+        rgbColor = rgbColor.concat(parseInt(hex.slice(5,7), 16), ",");
+        rgbColor = rgbColor.concat(opacity, ")");
 
-    gradient.append("svg:stop")
-    .attr("offset", "0%")
-    .attr("stop-color", this.colorPoint)
-    .attr("stop-opacity", 1);
+        return rgbColor
+    };
+    var gradient = this.context.createRadialGradient(this.sizePoint / 2, this.sizePoint / 2, 0, this.sizePoint / 2,this.sizePoint / 2,this.sizePoint/2);
+    gradient.addColorStop(0, colorToRgba(this.colorPoint, 1));
+    gradient.addColorStop(0.25,colorToRgba(this.colorPoint, 0.1));
+    gradient.addColorStop(0.75,colorToRgba(this.colorPoint, 0.05));
+    gradient.addColorStop(1,colorToRgba(this.colorPoint, 0));
 
-    gradient.append("svg:stop")
-    .attr("offset", "25%")
-    .attr("stop-color", this.colorPoint)
-    .attr("stop-opacity", 0.1);
+    return gradient
 
-    gradient.append("svg:stop")
-    .attr("offset", "75%")
-    .attr("stop-color", this.colorPoint)
-    .attr("stop-opacity", 0.05);
+};
 
-    gradient.append("svg:stop")
-    .attr("offset", "100%")
-    .attr("stop-color", this.colorPoint)
-    .attr("stop-opacity", 0);
+SpiralCreator.prototype.createStarImage = function(gradient) {
+    var starCanvas = document.createElement('canvas'); // offscreen canvas to create a star
+    starCanvas.width = this.sizePoint * 2;
+    starCanvas.height = this.sizePoint * 2;
+    var starContext = starCanvas.getContext("2d");
+
+    starContext.fillStyle = gradient;
+    starContext.arc(this.sizePoint, this.sizePoint, this.sizePoint, 0, 2*Math.PI, true);
+    starContext.fill();
+
+    return starCanvas;
 };
 
 SpiralCreator.prototype.displayStars = function () {
-    var starsPosition = getStarsPosition(this.params);
-    var data = starsPosition;
+    var data = getStarsPosition(this.params);
 
-    this.myGalaxyDiv.selectAll("svg").data([]).exit().remove();
-    var myGalaxySvg = this.myGalaxyDiv.append("svg")
-    .attr("width", this.widthCurve).attr("height", this.heightCurve)
-    .attr("style", "background-color: " + this.bgColor);
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.width);
 
-    this.createGradientColor(myGalaxySvg);
-
-    var circle = myGalaxySvg.selectAll("circle").data(data).enter().append("circle");
+    var gradient = this.createGradientColor();
+    var starImage = this.createStarImage(gradient);
 
     var xScale = d3.scaleLinear()
     .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
@@ -166,18 +170,14 @@ SpiralCreator.prototype.displayStars = function () {
     .domain([d3.min(data, d => d.y), d3.max(data, d => d.y)])
     .range([this.margin,this.heightCurve - this.margin]);
 
-    circle.attr("r", this.sizePoint)
-    .attr("cx", d => xScale(d.x))
-    .attr("cy", d => yScale(d.y))
-    .attr("fill", "url(#gradientStar)");
+
+    var obj = this;
+    data.forEach(function(d) {
+        obj.context.drawImage(starImage, xScale(d.x), yScale(d.y));
+    });
+    this.context.fill();
 
 };
-
-SpiralCreator.prototype.redraw = function() {
-    this.updateData();
-    Plotly.redraw(this.myGalaxyDiv);
-};
-
 
 SpiralCreator.prototype.displayParams = function() {
     var obj = this;
