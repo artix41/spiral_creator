5491e6dcaa83b326f2922129eb2256d71a927c3c
Pesah Arthur <arthur.pesah@ensta-paristech.fr>
2017-03-19 18:38:35 +0100

    Animate the 3D galaxy and use a texture

--- build/spiral.js
+++ build/spiral.js
@@ -5,25 +5,50 @@
 }(this, (function (exports) { 'use strict';
 
 function getStarsPosition(params) {
+    var t0 = performance.now();
     var positions = [];
     for (var iTraj = 0; iTraj < params.nbrEllipses.value; iTraj++) {
         var a = iTraj + 1;
         var b = params.e.value * (iTraj + 1);
         var angle = (iTraj / params.nbrEllipses.value) * Math.PI * 2;
-        var X = _.range(params.nbrStarsInEllipse.value).map(() => getStarOnEllipse(a, b, angle, params.noise.value));
+        var X = _.range(params.nbrStarsInEllipse.value).map(() => generateStarOnEllipse(a, b, angle, params.noise.value));
         positions = positions.concat(X);
     }
+    console.log(performance.now() - t0 + " ms");
     return positions;
 }
 
-function getStarOnEllipse(a, b, angle, noise) {
+function generateStarOnEllipse(a, b, angle, noise) {
     var t = Math.random() * 2 * Math.PI;
-    var X = [a * Math.cos(t), b * Math.sin(t)];
+    var noisyA = a + rnorm(0,noise);
+    var noisyB = b + rnorm(0,noise);
+    var X = [noisyA * Math.cos(t), noisyB * Math.sin(t)];
     var A = [[Math.cos(angle), -Math.sin(angle)], [Math.sin(angle), Math.cos(angle)]];
     var position = numeric.dot(A,X);
 
-    return {x: position[0] + rnorm(0,noise), y: position[1] + rnorm(0,noise)};
+    return {x: position[0], y: position[1], t: t, a: noisyA, b: noisyB, angle: angle};
 }
+
+function Star(sprite, t, a, b, angle) {
+    this.sprite = sprite;
+    this.t = t;
+    this.a = a;
+    this.b = b;
+    this.angle = angle;
+}
+
+Star.prototype.getPosition = function() {
+    var X = [this.a * Math.cos(this.t), this.b * Math.sin(this.t)];
+    var A = [[Math.cos(this.angle), -Math.sin(this.angle)], [Math.sin(this.angle), Math.cos(this.angle)]];
+    var position = numeric.dot(A,X);
+
+    return {x: position[0], y: position[1]};
+};
+
+Star.prototype.update = function (speed) {
+    this.t += speed;
+    this.t %= 2 * Math.PI;
+};
 
 function Slider(div, domain, callback, params) {
     // Code by Ben Frederickson in fmin
@@ -1231,7 +1256,8 @@ function SpiralCreator3D(div) {
     this.widthGalaxy = this.myGalaxyDiv.offsetWidth;
     this.heightGalaxy = 400;
     this.colorStar = "#DBE3EB";
-    this.radiusStar = 1;
+    this.radiusStar = 30;
+    this.speed = 0.1;
 
     this.params =   {
         e: {label: "Excentricity", value: 1.3, range:[0.1, 10], scale: d3.scaleLog(), ticks: 2, decimals: 2},
@@ -1240,6 +1266,30 @@ function SpiralCreator3D(div) {
         nbrEllipses: {label: "Number of ellipses", value: 50, range: [10, 100], scale: d3.scaleLinear(), ticks: 10, decimals: 0}
     };
 
+    /*var colorToRgba = function(hex, opacity) {
+        var rgbColor = "rgba("
+        rgbColor = rgbColor.concat(parseInt(hex.slice(1,3), 16), ",");
+        rgbColor = rgbColor.concat(parseInt(hex.slice(3,5), 16), ",");
+        rgbColor = rgbColor.concat(parseInt(hex.slice(5,7), 16), ",");
+        rgbColor = rgbColor.concat(opacity, ")");
+
+        return rgbColor
+    };
+
+    var c=document.getElementById("canvas");
+
+    var ctx=c.getContext("2d");
+
+    var gradient=ctx.createRadialGradient(this.radiusStar / 2, this.radiusStar / 2, 0, this.radiusStar / 2,this.radiusStar / 2,this.radiusStar/2);
+    console.log(colorToRgba(this.colorStar, 1))
+    gradient.addColorStop(0, colorToRgba(this.colorStar, 1));
+    gradient.addColorStop(0.25,colorToRgba(this.colorStar, 0.1));
+    gradient.addColorStop(0.75,colorToRgba(this.colorStar, 0.05));
+    gradient.addColorStop(1,colorToRgba(this.colorStar, 0));
+
+    ctx.fillStyle=gradient;
+    ctx.fillRect(10,10,150,100);*/
+
     this.displayParams();
     this.initRenderer();
     this.initStars();
@@ -1247,8 +1297,11 @@ function SpiralCreator3D(div) {
     var obj = this;
     function animate() {
         requestAnimationFrame(animate);
-        //obj.stars[1].rotation.x += 0.01;
-        //obj.stars[1].rotation.y += 0.02;
+        for(var i = 0; i < obj.stars.length; i++) {
+            obj.stars[i].update(obj.speed);
+            obj.stars[i].sprite.position.x = obj.xScale(obj.stars[i].getPosition().x);
+            obj.stars[i].sprite.position.y = obj.yScale(obj.stars[i].getPosition().y);
+        }
         obj.renderer.render(obj.scene, obj.camera);
     }
     this.renderer.render(this.scene, this.camera);
@@ -1273,11 +1326,11 @@ SpiralCreator3D.prototype.initRenderer = function(){
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(50, this.widthGalaxy / this.heightGalaxy, 1, 10000);
-    this.camera.position.set(0, 0, 1000);
+    this.camera.position.set(0, 0, 500);
     this.scene.add(this.camera);
 
-    var light = new THREE.DirectionalLight( 0xffffff, 1.0 );
-    light.position.set( 0, 0, 400 );
+    var light = new THREE.AmbientLight( 0xffffff);
+    //light.position.set( 0, 0, 400 );
     this.scene.add(light);
 
     var controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -1286,36 +1339,33 @@ SpiralCreator3D.prototype.initRenderer = function(){
 };
 
 SpiralCreator3D.prototype.initStars = function() {
-    //var texture = new THREE.TextureLoader().load("sprite.png");
-    var material = new THREE.SpriteMaterial({ color: 0xffffff, fog: true });
-    var sprite = new THREE.Sprite(material);
-
-    // var geometry = new THREE.SphereGeometry(this.radiusStar, 32, 32);
-    // var material = new THREE.MeshBasicMaterial({
-    //     color:this.colorStar,
-    //     //wireframe: true
-    // });
+    //var spriteMap = new THREE.Texture(starImage)
+    var spriteMap = new THREE.TextureLoader().load("particle.png");
+    var material = new THREE.SpriteMaterial({ map: spriteMap, color: 0xffffff, fog: true, lights: true });
 
     var data = getStarsPosition(this.params);
     this.clearScene(data.length);
 
-    var xScale = d3.scaleLinear()
+    this.xScale = d3.scaleLinear()
     .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
     .range([-200,200]);
 
-    var yScale = d3.scaleLinear()
+    this.yScale = d3.scaleLinear()
     .domain([d3.min(data, d => d.y), d3.max(data, d => d.y)])
     .range([-200,200]);
 
     this.stars = [];
     var obj = this;
+    var t0 = performance.now();
     data.forEach(function(d, i) {
-        obj.stars.push(new THREE.Sprite(material));
-        obj.stars[i].position.x = xScale(d.x);
-        obj.stars[i].position.y = yScale(d.y);
-        obj.stars[i].name = "star" + i;
-        obj.scene.add(obj.stars[i]);
+        obj.stars.push(new Star(new THREE.Sprite(material), d.t, d.a, d.b, d.angle));
+        obj.stars[i].sprite.position.x = obj.xScale(d.x);
+        obj.stars[i].sprite.position.y = obj.yScale(d.y);
+        obj.stars[i].sprite.scale.set(5,5,1);
+        obj.stars[i].sprite.name = "star" + i;
+        obj.scene.add(obj.stars[i].sprite);
     });
+    console.log("Total : ",  performance.now() - t0 + " ms");
 };
 
 SpiralCreator3D.prototype.displayParams = function() {
--- src/spiral_generator.js
+++ src/spiral_generator.js
@@ -1,20 +1,45 @@
 export function getStarsPosition(params) {
+    var t0 = performance.now();
     var positions = [];
     for (var iTraj = 0; iTraj < params.nbrEllipses.value; iTraj++) {
         var a = iTraj + 1;
         var b = params.e.value * (iTraj + 1);
         var angle = (iTraj / params.nbrEllipses.value) * Math.PI * 2;
-        var X = _.range(params.nbrStarsInEllipse.value).map(() => getStarOnEllipse(a, b, angle, params.noise.value));
+        var X = _.range(params.nbrStarsInEllipse.value).map(() => generateStarOnEllipse(a, b, angle, params.noise.value));
         positions = positions.concat(X)
     }
+    console.log(performance.now() - t0 + " ms")
     return positions;
 }
 
-function getStarOnEllipse(a, b, angle, noise) {
+function generateStarOnEllipse(a, b, angle, noise) {
     var t = Math.random() * 2 * Math.PI;
-    var X = [a * Math.cos(t), b * Math.sin(t)];
+    var noisyA = a + rnorm(0,noise);
+    var noisyB = b + rnorm(0,noise);
+    var X = [noisyA * Math.cos(t), noisyB * Math.sin(t)];
     var A = [[Math.cos(angle), -Math.sin(angle)], [Math.sin(angle), Math.cos(angle)]];
     var position = numeric.dot(A,X);
 
-    return {x: position[0] + rnorm(0,noise), y: position[1] + rnorm(0,noise)};
+    return {x: position[0], y: position[1], t: t, a: noisyA, b: noisyB, angle: angle};
 }
+
+export function Star(sprite, t, a, b, angle) {
+    this.sprite = sprite;
+    this.t = t;
+    this.a = a;
+    this.b = b;
+    this.angle = angle;
+}
+
+Star.prototype.getPosition = function() {
+    var X = [this.a * Math.cos(this.t), this.b * Math.sin(this.t)];
+    var A = [[Math.cos(this.angle), -Math.sin(this.angle)], [Math.sin(this.angle), Math.cos(this.angle)]];
+    var position = numeric.dot(A,X);
+
+    return {x: position[0], y: position[1]};
+}
+
+Star.prototype.update = function (speed) {
+    this.t += speed;
+    this.t %= 2 * Math.PI;
+};
